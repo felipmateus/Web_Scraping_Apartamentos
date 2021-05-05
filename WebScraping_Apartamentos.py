c41bbdb4cbcd61ed9a95cdcd4e527bd3cf54cfec
felipmateus <76415936+felipmateus@users.noreply.github.com>
2021-05-04 22:45:39 -0300

    correção no print das informações raspadas

--- WebScraping_Apartamentos.py
+++ WebScraping_Apartamentos.py
@@ -7,7 +7,7 @@ from time import sleep
 
 
 def get_system():
-
+##   Nessa parte eu entro com os parâmetros de pesquisa do apartamento
     driver: WebDriver = webdriver.Chrome()
     url = "https://www.zapimoveis.com.br/"
     driver.get(url)
@@ -16,20 +16,26 @@ def get_system():
     driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/div/section/form/div/div[2]/div/div/div/input').send_keys('Niterói, Icarai')
     driver.find_element_by_xpath('//*[@id="app"]/section/section[1]/div/section/form/div/div[2]/button').send_keys(Keys.ENTER)
 
-    ## a função eleep é necessária devido à demora do website para carregar, causando erro caso não o tenha no código
+##  a função eleep é necessária devido à demora para carregar o website, causando erro caso não o tenha no código
     sleep(2)
 
+##  foi necessário implementar um contador para driblar o site para conseguir localizar o botão de próxima página
     cont = 5
 
-    for i in range(5):
+## o primeiro laço de repetição é para direcionar para próxima página
+    for i in range(20):
+##  o sengundo laço é para raspar todas as informações essenciais da página
+        for i in range(20):
+            dados = driver.find_element_by_xpath('//*[@id="app"]/section/div[3]/div[2]/section/div/div[1]/div/div[2]')
+            print(dados.text)
         cont = cont + 1
+##  foi necessário colocar uma condição para driblar o site e localizar o botão próxima página
         if cont>7:
             cont = cont - 1
-
-        print(cont)
+            print('PRÓXIMA PÁGINA')
+            driver.find_element_by_xpath('//*[@id="app"]/section/div[3]/div[2]/section/ul/li['+str(cont)+']/button').send_keys(Keys.ENTER)
+        else:
+            driver.find_element_by_xpath('//*[@id="app"]/section/div[3]/div[2]/section/ul/li[' + str(cont) + ']/button').send_keys(Keys.ENTER)
         sleep(5)
-        driver.find_element_by_xpath('//*[@id="app"]/section/div[3]/div[2]/section/ul/li['+str(cont)+']/button').send_keys(Keys.ENTER)
-
-    sleep(10)
 
 get_system()

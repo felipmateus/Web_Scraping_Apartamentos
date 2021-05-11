f88386a562ddfa89e08630b3a74dce73fb3efd03
felipmateus <76415936+felipmateus@users.noreply.github.com>
2021-05-10 22:36:44 -0300

    Nova função: adicionar as informações em uma planilha (ainda falta corrigir alguns erros)

--- WebScraping_Apartamentos.py
+++ WebScraping_Apartamentos.py
@@ -1,9 +1,10 @@
-## Foi necessário implementar um contador para driblar o site e conseguir localizar o botão da próxima página.
+import csv
 
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
+
 
 
 class scrappy:
@@ -20,13 +21,52 @@ class scrappy:
 
     def raspagem_dados(self):
         cont = 5
-        for i in range(20):
-            for i in range(20):
-                dados = self.driver.find_element_by_xpath('//*[@id="app"]/section/div[3]/div[2]/section/div/div[1]/div/div[2]')
-                print(dados.text)
+        for i in range(24):
 
+            valores_aluguel = self.driver.find_elements_by_xpath('//p[@class="simple-card__price js-price heading-regular heading-regular__bolder align-left"]')
+            for valor_aluguel in valores_aluguel:
+                self.valores_aluguel_corrigido = valor_aluguel.text.split('R$')[1].split('/')[0]
 
+            sleep(1)
+            valores_iptu = self.driver.find_elements_by_xpath("//li[@class='card-price__item iptu text-regular']")
+            for valor_iptu in valores_iptu:
+                self.valores_iptu_corrigido = valor_iptu.text.split('R$')[1]
+            sleep(1)
 
+            descricoes_apartamentos = self.driver.find_elements_by_xpath('//div[@class="collapse simple-card__description"]')
+            for descricao_apartamento in descricoes_apartamentos:
+                self.descricoes = descricao_apartamento.text
+            sleep(1)
+
+            valores_condominio = self.driver.find_elements_by_xpath('//li[@class="card-price__item condominium text-regular"]')
+            for valor_condominio in valores_condominio:
+                self.valores_condominio_corrigidos = valor_condominio.text.aplit('R$')[1]
+            sleep(1)
+
+            enderecos = self.driver.find_elements_by_xpath('//p[@class="color-dark text-regular simple-card__address"]')
+            for endereco in enderecos:
+                self.enderecos_corrigidos = endereco.text
+            sleep(1)
+
+            area_apartamentos = self.driver.find_elements_by_xpath('//li[@class="feature__item text-small js-areas"]')
+            for area_apartamento in area_apartamentos:
+                self.area_apartamentos_corrigidos = area_apartamento.text.split('m²')[0]
+            sleep(1)
+
+            numero_quartos = self.driver.find_elements_by_xpath('//li[@class="feature__item text-small js-bedrooms"]')
+            for numero_quarto in numero_quartos:
+                self.numero_quartos_corrigidos = numero_quarto.text
+            sleep(1)
+
+            numero_vagas = self.driver.find_elements_by_xpath('//li[@class="feature__item text-small js-parking-spaces"]')
+            for numero_vaga in numero_vagas:
+                self.numero_vagas_corrigidas = numero_vaga.text
+            sleep(1)
+
+            numero_banheiros = self.driver.find_elements_by_xpath('//li[@class="feature__item text-small js-bathrooms"]')
+            for numero_banheiro in numero_banheiros:
+                self.numero_banheiros_corrigidos = numero_banheiro.text
+            sleep(1)
 
             cont = cont + 1
             if cont>7:
@@ -37,8 +77,25 @@ class scrappy:
                 self.driver.find_element_by_xpath('//*[@id="app"]/section/div[3]/div[2]/section/ul/li[' + str(cont) + ']/button').send_keys(Keys.ENTER)
             sleep(5)
 
+    def criar_planilha(self):
+        with open(r'C:\Users\Felipe\PycharmProjects\webscraping_apartamento\protagoniste.csv', 'w', newline='') as result_file:
+            wr = csv.writer(result_file, dialect='excel')
+            wr.writerow(self.valores_aluguel_corrigido)
+            wr.writerow(self.descricao)
+            wr.writerow(self.valores_condominio_corrigidos)
+            wr.writerow(self.valores_iptu_corrigido)
+            wr.writerow(self.enderecos_corrigidos)
+            wr.writerow(self.area_apartamentos_corrigidos)
+            wr.writerow(self.numero_quartos_corrigidos)
+            wr.writerow(self.numero_vagas_corrigidas)
+            wr.writerow(self.numero_banheiros_corrigidos)
+
+
+
+
+
+
 start = scrappy()
 start.iniciar()
 start.raspagem_dados()
-
-
+start.criar_planilha()
